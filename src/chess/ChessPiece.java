cec5718a6577fefbc9d16f9db5e6125f421f6f4d
Marcos Vinicius <marcos.viniciuscruz00@gmail.com>
2023-07-13 21:06:28 -0300

    check logic

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -17,6 +17,10 @@ public abstract class ChessPiece extends Piece {
         return color;
     }
 
+    public ChessPosition getChessPosition(){
+        return ChessPosition.fromPosition(position);
+    }
+
     public abstract String toString();
 
     protected boolean isThereOpponentPiece(Position position){
