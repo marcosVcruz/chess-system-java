8fb85a8491ba4326bdb319e1994749c155998187
Marcos Vinicius <marcos.viniciuscruz00@gmail.com>
2023-07-18 07:16:29 -0300

    Queen

--- src/chess/chess/pieces/Pawn.java
+++ src/chess/chess/pieces/Pawn.java
@@ -12,6 +12,11 @@ public class Pawn extends ChessPiece {
     }
 
     @Override
+    public String toString() {
+        return "P";
+    }
+
+    @Override
     public boolean[][] possibleMoves() {
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 
@@ -56,10 +61,5 @@ public class Pawn extends ChessPiece {
             }
         }
         return mat;
-    }
-
-    @Override
-    public String toString() {
-        return "P";
     }
 }
